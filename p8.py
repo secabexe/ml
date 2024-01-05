70dd9d4cac1a8f8d655bc58e952ac2c19d96ef24
zencodee <112794663+zencodee@users.noreply.github.com>
2024-01-05 16:28:16 +0530

    Update p8.py

--- p8.py
+++ p8.py
@@ -1,17 +1,15 @@
-from sklearn.model_selection import train_test_split 
-from sklearn.neighbors import KNeighborsClassifier 
 from sklearn import datasets
-iris=datasets.load_iris() 
-print("Iris Data set loaded...")
-x_train, x_test, y_train, y_test = train_test_split(iris.data,iris.target,test_size=0.1)
-#random_state=0
-for i in range(len(iris.target_names)):
-    print("Label", i , "-",str(iris.target_names[i]))
-classifier = KNeighborsClassifier(n_neighbors=5)
-classifier.fit(x_train, y_train)
+iris=datasets.load_iris()
+iris_data=iris.data
+iris_labels=iris.target
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(iris_data,iris_labels,test_size=0.30)
+from sklearn.neighbors import KNeighborsClassifier
+classifier=KNeighborsClassifier(n_neighbors=5)
+classifier.fit(x_train,y_train)
 y_pred=classifier.predict(x_test)
-print("Results of Classification using K-nn with K=5 ") 
-for r in range(0,len(x_test)):
-    print(" Sample:", str(x_test[r]), " Actual-label:", str(y_test[r])," Predicted-label:", str(y_pred[r]))
-
-    print("Classification Accuracy :" , classifier.score(x_test,y_test));
+from sklearn.metrics import classification_report,confusion_matrix
+print('Confusion matrix is as follows')
+print(confusion_matrix(y_test,y_pred))
+print('Accuracy Matrics')
+print(classification_report(y_test,y_pred))
